e6f2c367a8f92121388be674bae6b75f070422a4
Rodolpho Caetano <rphfti@hotmail.com>
2018-12-20 21:08:47 +0000

    Andamento aula 12

---
+++ js/photo-store.js
@@ -0,0 +1,8 @@
+/*
+Ter a janela (browser) na mão
+Ter HTML na mão
+Pegar o coração
+Pegar clique no coração
+No momento que o usuario clicar, nós queremos mostrar um texto AE na tela
+*/
+const $heart = window.document.querySelector(".heart");
