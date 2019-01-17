b9c9410df2b0c3b98a73ff4b72ff44bc80950644
Rodolpho Caetano <rphfti@hotmail.com>
2019-01-17 19:51:56 +0000

    resolvendo desafios

--- js/header-store.js
+++ js/header-store.js
@@ -1,6 +1,12 @@
 const $search = document.querySelector(".search");
+const $searchIcon = document.querySelector(".searchIcon");
 
 $search.addEventListener("click", () => {
-  //arrow function
+  $search.classList.toggle("-active");
+  $searchIcon.classList.toggle("-active");
+}); //arrow function
+
+$searchIcon.addEventListener("click", () => {
+  $searchIcon.classList.toggle("-active");
   $search.classList.toggle("-active");
 });
